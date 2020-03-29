2d2f8db735fc15b7725fd9cb75a601c378c90da7
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-03-29 11:11:12 +0300

    root query

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // bind express with graphql
-
 app.use(
 	'/graphql',
 	graphqlHTTP({
 		// pass in a schema property
+		schema,
 	}),
 );
 
