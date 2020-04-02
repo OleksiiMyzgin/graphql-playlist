4ffed45dd8469c4fe62c6fa85f853703936b102b
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-02 12:36:58 +0300

    Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect(
+	'mongodb://oleksii:test123@ds055397.mlab.com:55397/graphql-playlist-ninja',
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+);
+mongoose.connection.once('open', () => {
+	console.log('conneted to database');
+});
 
 // bind express with graphql
 app.use(
