db3e6f213484a6866801b73f4850cdf7a844ee49
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-05 11:12:11 +0300

    Making Queries from React

--- client/src/components/BookList.tsx
+++ client/src/components/BookList.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+type Book = {
+  id : string;
+  name : string;
+}
+
+type Books = {
+  books: Book[]
+}
+
+const GET_BOOKS_QUERY = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 function BookList() {
+  const { loading, error, data } = useQuery<Books>(GET_BOOKS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <ul className="book-list">
-        <li>Book name</li>
+        {data && data.books.map(book => (
+          <li key={book.id}>{book.name}</li>
+        ))}
       </ul>
     </div>
   );
