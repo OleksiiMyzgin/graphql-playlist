a2c9727eb90f996049c59ed9ecd42ab4a88d6280
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-05 12:12:15 +0300

    updating component state

--- client/src/components/AddBook.tsx
+++ client/src/components/AddBook.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_AUTHORS_QUERY } from '../queries'
@@ -13,23 +13,32 @@ type AuthorsList = {
 }
 
 function AddBook() {
+  const [name, setName] = useState('');
+  const [genre, setGenre] = useState('');
+  const [authorId, setAuthorId] = useState('');
+
   const { loading, error, data } = useQuery<AuthorsList>(GET_AUTHORS_QUERY);
 
   if (error) return <p>Error: {error.message}</p>;
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(name, genre, authorId);
+  }
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={handleSubmit}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setName(e.currentTarget.value)} />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setGenre(e.currentTarget.value)} />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select onChange={(e) => setAuthorId(e.currentTarget.value)}>
           <option>Select author</option>
           {loading ? (
             <option disabled>Loading authors</option>
