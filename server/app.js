9e7c1b40d6f8cdec3aa995d23fc3fcf0aaf4714b
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-05 11:12:04 +0300

    add cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const schema = require('./schema/schema');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect(
