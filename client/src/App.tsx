7e79dcd6a12e402eb0762dc632ae1383e7764f48
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-08 12:55:16 +0300

    fixed code styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-import BookList from './components/BookList'
-import AddBook from './components/AddBook'
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: "http://localhost:4000/graphql",
 });
 
 function App() {
--- client/src/components/BookList.tsx
+++ client/src/components/BookList.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
 
-import { GET_BOOKS_QUERY } from '../queries'
+import { GET_BOOKS_QUERY } from "../queries";
+import { BookDetails } from "./BookDetails";
 
 type Book = {
-  id : string;
-  name : string;
-}
+  id: string;
+  name: string;
+};
 
 type BooksList = {
-  books: Book[]
-}
+  books: Book[];
+};
 
 function BookList() {
+  const [bookId, setBookId] = useState<string | null>(null);
   const { loading, error, data } = useQuery<BooksList>(GET_BOOKS_QUERY);
 
   if (loading) return <p>Loading...</p>;
@@ -21,10 +23,18 @@ function BookList() {
   return (
     <div>
       <ul className="book-list">
-        {data && data.books.map(book => (
-          <li key={book.id}>{book.name}</li>
-        ))}
+        {data &&
+          data.books.map((book) => (
+            <li key={book.id} onClick={(e) => setBookId(book.id)}>
+              {book.name}
+            </li>
+          ))}
       </ul>
+      {bookId ? (
+        <BookDetails bookId={bookId} />
+      ) : (
+        <div>No Book selected...</div>
+      )}
     </div>
   );
 }
